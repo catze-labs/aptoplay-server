31e362f08dfb5cc07c5b7d9189928e9f083eda7d
Phantola <devmonarch0115@gmail.com>
2023-01-31 17:27:13 +0900

    Add libraries and initial settings

--- src/routes/routes.module.ts
+++ src/routes/routes.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ServicesModule } from "src/services/services.module";
+import { AuthController } from './auth/auth.controller';
+import { UserController } from './user/user.controller';
+import { NonceController } from './nonce/nonce.controller';
 
 @Module({
   imports: [ServicesModule],
-  controllers: []
+  controllers: [AuthController, UserController, NonceController]
 })
 export class RoutesModule {}
--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma/prisma.service";
 import { AptoplayService } from "./aptoplay/aptoplay.service";
+import { ScheduleService } from './schedule/schedule.service';
 
 @Module({
-  providers: [PrismaService, AptoplayService],
+  providers: [PrismaService, AptoplayService, ScheduleService],
   exports: [PrismaService, AptoplayService]
 })
 export class ServicesModule {}
