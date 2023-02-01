9e6e01c2d4ae8d21eaee31ceb31ed69eeb9e7a4c
Phantola <devmonarch0115@gmail.com>
2023-02-01 13:01:58 +0900

    Add Transaction service

--- src/services/services.module.ts
+++ src/services/services.module.ts
@@ -3,9 +3,16 @@ import { PrismaService } from "./prisma/prisma.service";
 import { AptoplayService } from "./aptoplay/aptoplay.service";
 import { ScheduleService } from "./schedule/schedule.service";
 import { AuthService } from "./auth/auth.service";
+import { TransactionService } from "./transaction/transaction.service";
 
 @Module({
-  providers: [PrismaService, AptoplayService, ScheduleService, AuthService],
-  exports: [PrismaService, AptoplayService, AuthService]
+  providers: [
+    PrismaService,
+    AptoplayService,
+    ScheduleService,
+    AuthService,
+    TransactionService
+  ],
+  exports: [PrismaService, AptoplayService, AuthService, TransactionService]
 })
 export class ServicesModule {}
